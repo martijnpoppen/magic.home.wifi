6b67776095a3e27631d3d8e975691fa179ab1d13
Martijn Poppen <m.poppen@ziggo.nl>
2021-11-08 17:43:00 +0100

    FIX: various devices broken after update

--- constants/index.js
+++ constants/index.js
@@ -7,10 +7,10 @@ exports.typeCapabilityMap = {
     'AK001-ZJ2145'    : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'light_temperature', 'light_mode' ],
     'AK001-ZJ2147'    : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'light_temperature', 'light_mode' ],
     'AK001-ZJ2148'    : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'addressable' ],
-    'HF-LPB100'       : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'addressable' ],
-    'HF-LPB100-0'     : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'addressable' ],
-    'HF-LPB100-1'     : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'addressable' ],
-    'HF-LPB100-ZJ002' : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'addressable' ],
-    'HF-LPB100-ZJ200' : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'addressable' ],
+    'HF-LPB100-0'     : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'light_temperature', 'light_mode' ],
+    'HF-LPB100-1'     : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'light_temperature', 'light_mode' ],
+    'HF-LPB100'       : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'light_temperature', 'light_mode' ],
+    'HF-LPB100-ZJ002' : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'light_temperature', 'light_mode' ],
+    'HF-LPB100-ZJ200' : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'light_temperature', 'light_mode' ],
     'other'           : [ 'onoff', 'dim', 'light_hue', 'light_saturation', 'light_temperature', 'light_mode' ]
 };
