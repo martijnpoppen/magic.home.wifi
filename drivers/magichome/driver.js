82eb8159986b5be49813c60367ba83cebaee52db
Martijn Poppen <m.poppen@ziggo.nl>
2021-11-08 21:15:50 +0100

    FIX: ZJ2104

--- drivers/magichome/driver.js
+++ drivers/magichome/driver.js
@@ -14,9 +14,9 @@ class MagicHomeDriver extends Homey.Driver {
       for (let i in discovery.clients.filter((v,i,a)=>a.findIndex(t=>(t.id === v.id))===i)) {
         if (result[i].model == 'AK001-ZJ100' || result[i].model == 'AK001-ZJ2101') {
           var name = 'RGB controller '+ result[i].model +' ('+ result[i].address +')';
-        } else if (result[i].model == 'AK001-ZJ210' || result[i].model == 'AK001-ZJ200' || result[i].model == 'AK001-ZJ2148') {
+        } else if (result[i].model == 'AK001-ZJ210' || result[i].model == 'AK001-ZJ2148') {
           var name = 'RGB SPI addressable controller '+ result[i].model +' ('+ result[i].address +')';
-        } else if (result[i].model == 'HF-LPB100' || result[i].model == 'HF-LPB100-0' || result[i].model == 'HF-LPB100-1' || result[i].model == 'HF-LPB100-ZJ002' || result[i].model == 'HF-LPB100-ZJ200' || result[i].model == 'AK001-ZJ2104' || result[i].model == 'AK001-ZJ2145' || result[i].model == 'AK001-ZJ2147') {
+        } else if (result[i].model == 'AK001-ZJ200' || result[i].model == 'HF-LPB100' || result[i].model == 'HF-LPB100-0' || result[i].model == 'HF-LPB100-1' || result[i].model == 'HF-LPB100-ZJ002' || result[i].model == 'HF-LPB100-ZJ200' || result[i].model == 'AK001-ZJ2104' || result[i].model == 'AK001-ZJ2145' || result[i].model == 'AK001-ZJ2147') {
           var name = 'RGBWW controller '+ result[i].model +' ('+ result[i].address +')';
         } else {
           var name = result[i].model +' ('+ result[i].address +')';
